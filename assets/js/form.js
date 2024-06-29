0dc6c63c3682db152829fa9967742ed94f69289a
Lauren Coates <lspassey@gmail.com>
2024-06-29 09:07:24 -0700

    add a variable that selects the form element and add event listener for submit

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,4 +1,5 @@
 // TODO: Create a variable that selects the form element
+const form = document.getElementById('form');
 
 // TODO: Create a function that handles the form submission. Grab the form data and store it in local storage, then redirect to the blog page using the redirectPage function. If the form is submitted with missing data, display an error message to the user.
 
@@ -10,3 +11,7 @@ const redirectPage = function (url) {
 };
 
 // TODO: Add an event listener to the form on submit. Call the function to handle the form submission.
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+});
