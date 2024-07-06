834728a10d2d06d5904159f827afcf167a81c2e7
Lauren Coates <lspassey@gmail.com>
2024-07-05 18:54:27 -0700

    add section and aside elements

--- assets/js/form.js
+++ assets/js/form.js
@@ -5,8 +5,6 @@ const titleEl = document.getElementById('title')
 const contentEl = document.getElementById('content')
 // TODO: Create a function that handles the form submission. Grab the form data and store it in local storage, then redirect to the blog page using the redirectPage function. If the form is submitted with missing data, display an error message to the user.
 
-let redirectURL = '';
-
 
 function pageSubmission(event) {
   event.preventDefault();
