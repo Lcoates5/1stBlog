2b1f6c67b412182a513529eeb92fb8eb05c1d14a
Lauren Coates <lspassey@gmail.com>
2024-07-06 14:27:36 -0700

    add functions to create blog post, and handle no blog posts to display

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,10 +1,40 @@
 // TODO: Create a variable that selects the main element
 const main = document.querySelector('main')
-
 // TODO: Create a function that builds an element and appends it to the DOM
-
+function createBlogPost() {
+    console.log("creating new blog post") 
+    const blogPosts = readBlogPostStorage()
+    console.log(blogPosts)
+    
+    for (let index = 0; index < blogPosts.length; index++) {
+        const currentBlogPost = blogPosts[index];
+        const blogPostElem = document.createElement("article")
+        const blogPostTitle = document.createElement('h3')
+        blogPostTitle.textContent = currentBlogPost.blogPostTitle
+        blogPostElem.appendChild(blogPostTitle)
+    }
+    main.appendChild(blogPost)
+}
 // TODO: Create a function that handles the case where there are no blog posts to display
-
+function handleNoBlogPosts(event) {
+    console.log("No blog posts to display") 
+    event.preventDefault();
+    
+    if (blogPosts.length <= 0) {
+        const errorMessage = document.createElement('p');
+        errorMessage.textContent = 'No blog posts to display';
+        main.appendChild(errorMessage);
+        
+    }
+}
 // TODO: Create a function that reads from local storage and returns the data
-
+function readBlogPostStorage() {
+    console.log("Reading blog post storage") 
+    
+    const blogPosts = JSON.parse(localStorage.getItem('blogPosts')) || []
+  console.log(blogPosts)
+  return blogPosts  
+}
 // TODO: Call the function to render the list of blog posts
+
+window.addEventListener('load', handleNoBlogPosts)
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,28 +1,19 @@
 // TODO: Create a variable that selects the form element
-const form = document.getElementById('form');
+const form = document.getElementById('blog-form');
 const usernameEl = document.getElementById('username')
 const titleEl = document.getElementById('title')
 const contentEl = document.getElementById('content')
 // TODO: Create a function that handles the form submission. Grab the form data and store it in local storage, then redirect to the blog page using the redirectPage function. If the form is submitted with missing data, display an error message to the user.
+document.body.classList.add('light-mode');
 
-
-function pageSubmission(event) {
+function handleSubmit(event) {
   event.preventDefault();
-}
+  console.log(event.currentTarget);
 
-const redirectPage = function (url) {
-  redirectURL = url;
-  location.assign(url);
-};
-
-// TODO: Add an event listener to the form on submit. Call the function to handle the form submission.
-
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
   let formValue = {
-    username:usernameEl.value,
-    title:titleEl.value,
-    content:contentEl.value
+    username: usernameEl.value,
+    title: titleEl.value,
+    content: contentEl.value
   }
   if (formValue.username === '' || formValue.title === '' || formValue.content === '') {
     const errorMessage = document.getElementById('errormessage');
@@ -32,4 +23,13 @@ form.addEventListener('submit', function(event) {
 
   localStorage.setItem('form', JSON.stringify(formValue));
   redirectPage('blog.html');
-});
+}
+
+const redirectPage = function (url) {
+  redirectURL = url;
+  location.assign(url);
+};
+
+// TODO: Add an event listener to the form on submit. Call the function to handle the form submission.
+
+form.addEventListener('submit', handleSubmit);
