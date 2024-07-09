9d5ee753bc2dcdcfe5c16a14da5162961d2d99a7
Lauren Coates <lspassey@gmail.com>
2024-07-08 17:52:03 -0700

    fixed error with empty submit on index.html, also finished light/dark mode and changed circle color

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,35 +1,32 @@
 // TODO: Create a variable that selects the form element
 const form = document.getElementById('blog-form');
-const usernameEl = document.getElementById('username')
-const titleEl = document.getElementById('title')
-const contentEl = document.getElementById('content')
-// TODO: Create a function that handles the form submission. Grab the form data and store it in local storage, then redirect to the blog page using the redirectPage function. If the form is submitted with missing data, display an error message to the user.
+const usernameEl = document.getElementById('username');
+const titleEl = document.getElementById('title');
+const contentEl = document.getElementById('content');
 
+// TODO: Create a function that handles the form submission. Grab the form data and store it in local storage, then redirect to the blog page using the redirectPage function. If the form is submitted with missing data, display an error message to the user.
 function handleSubmit(event) {
   event.preventDefault();
   console.log(event.currentTarget);
 
   let formValue = {
-    username: usernameEl.value,
-    title: titleEl.value,
-    content: contentEl.value
-  }
-  let formvalues = JSON.parse(localStorage.getItem('formvalues')) || [];
-  formvalues.push(formValue);
-  localStorage.setItem('formvalues', JSON.stringify(formvalues));
-
-window.location.href = 'blog.html';
+    username: usernameEl.value.trim(),
+    title: titleEl.value.trim(),
+    content: contentEl.value.trim()
+  };
 
   if (formValue.username === '' || formValue.title === '' || formValue.content === '') {
     const errorMessage = document.getElementById('errormessage');
     errorMessage.innerText = 'Please complete the form';
-    return
+    return;
   }
 
-  localStorage.setItem('form', JSON.stringify(formValue));
-  redirectPage('blog.html');
+  let formvalues = JSON.parse(localStorage.getItem('formvalues')) || [];
+  formvalues.push(formValue);
+  localStorage.setItem('formvalues', JSON.stringify(formvalues));
+
+  window.location.href = 'blog.html';
 }
 
 // TODO: Add an event listener to the form on submit. Call the function to handle the form submission.
-
 form.addEventListener('submit', handleSubmit);
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,8 +1,21 @@
 // TODO: Create logic to toggle the light/dark mode styles for the page and circle. The mode should be saved to local storage.
+const lightDark = document.getElementById("toggle");
+const circle = document.getElementById("circle")
+
+lightDark.addEventListener("click", function () {
+console.log("Button was clicked")
+if (document.body.classList.contains("dark-mode")){
+    console.log("check class list for dark mode")
+    document.body.classList.remove("dark-mode");
+}
+else {
+    document.body.classList.add("dark-mode");
+    console.log("reset state")
+};
+})
 
 
 // TODO: Create a function called `readLocalStorage` that reads from local storage and returns the data. If no data exists, return an empty array.
-
 
 // TODO: Create a function called `storeLocalStorage` that takes a given object and saves the new data to the existing blog data in local storage.
 
